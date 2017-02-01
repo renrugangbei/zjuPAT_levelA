cbdb1f7ba1bb07f227d18e1d06fc699873c58573
renrugangbei <1183356044@qq.com>
2017-02-02 00:02:31 +0800

    2.1.17

--- 1004/1004.cpp
+++ 1004/1004.cpp
@@ -1,39 +1,41 @@
-#include <cstdio>
-#include <queue>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int level[100], book[100], maxlevel = -1;
-vector<int> v[100];
+vector<int> v[100];//v存放树，每一个父节点链接所有他的子节点，节点从1开始，按顺序编址
+int level[100], cnt[100], levelcnt = -1;//level 存储每个节点的层数，cnt存储每层有几个叶子节点，levelcnt存储有几层 
 void bfs() {
-    queue<int> q;
-    q.push(1);
-    level[1] = 0;
-    while(!q.empty()) {
-        int index = q.front();
-        q.pop();
-        maxlevel = max(level[index], maxlevel);
-        if(v[index].size() == 0)
-            book[level[index]]++;
-        for(int i = 0; i < v[index].size(); i++) {
-            q.push(v[index][i]);
-            level[v[index][i]] = level[index] + 1;
-        }
-    }
+	queue<int> q;
+	q.push(1);
+	level[1] = 0;
+	while (!q.empty()) {
+		int m = q.front();
+		q.pop();
+		levelcnt = max(level[m], levelcnt);
+		if (v[m].size() == 0) {
+			cnt[level[m]]++;//v[m].size()==0代表这个节点没有子节点
+		}
+		for (int i = 0; i < v[m].size(); i++) {
+			q.push(v[m][i]);//将下一层全部入队，做宽度优先遍历
+			level[v[m][i]] = level[m] + 1;//儿子的层数比父亲大一
+		}
+	}
 }
 int main() {
-    int n, m, k, node, c;
-    scanf("%d %d", &n, &m);
-    for(int i = 0; i < m; i++) {
-        scanf("%d %d",&node, &k);
-        for(int j = 0; j < k; j++) {
-            scanf("%d", &c);
-            v[node].push_back(c);
-        }
-    }
-    bfs();
-    printf("%d", book[0]);
-    for(int i = 1; i <= maxlevel; i++)
-        printf(" %d", book[i]);
-    return 0;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b,c;
+		cin >> a>>b;
+		for (int j = 0; j < b; j++) {
+			cin >> c;
+			v[a].push_back(c);
+		}
+	}
+	bfs();
+	cout << cnt[0];
+	for (int i = 1; i <= levelcnt; i++) {
+		cout << " " << cnt[i];
+	}
 }
