12381f4363883f967af6972ed0d74b3ced4bc614
renrugangbei <1183356044@qq.com>
2018-07-06 21:44:31 +0800

    7.6.18

--- 1001/1001.cpp
+++ 1001/1001.cpp
@@ -1,30 +1,14 @@
-#include<iostream>
-#include<stack>
-#include<iomanip>
+#include <iostream>
 using namespace std;
 int main() {
-	int input1, input2, num;
-	cin >> input1 >> input2;
-	num = input1 + input2;
-	if (num < 0) {
-		num = -num;
-		cout << "-";
-	}
-	stack<int> stk;
-	if (num == 0) {
-		stk.push(0);
-	}
-	else{
-		while (num != 0) {
-			stk.push(num % 1000);
-			num /= 1000;
-		}
-	}
-	cout << stk.top();
-	stk.pop();
-	while (!stk.empty()) {
-		cout << "," << setfill('0') << setw(3) << stk.top();//²¹Áã
-		stk.pop();
-	}
-	return 0;
+    int a, b;
+    cin >> a >> b;
+    string s = to_string(a + b);
+    int len = s.length();
+    for (int i = 0; i < len; i++) {
+        cout << s[i];
+        if (s[i] == '-') continue;
+        if ((i + 1) % 3 == len % 3 && i != len - 1) cout << ",";
+    }
+    return 0;
 }
