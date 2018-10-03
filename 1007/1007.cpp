3fb2f3c30a1ef89ce8e93c45212cb5a298d38266
renrugangbei <1183356044@qq.com>
2018-10-03 19:38:23 +0800

    10.3.18

--- 1007/1007.cpp
+++ 1007/1007.cpp
@@ -1,28 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 int main() {
-	int n, maxSum = -1, thisSum = 0, m1, end, start;
-	cin >> n;
-	int a[10005] = {0};
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	m1= 0;
-	for (int i = 0; i < n; i++) {
-		thisSum += a[i];
-		if (thisSum > maxSum) {
-			maxSum = thisSum;
-			end = i;
-			start = m1;;
-		} else if (thisSum < 0) {
-			thisSum = 0;
-			m1 = i + 1;;
-		}
-	}
-	if (maxSum != -1) {
-		cout << maxSum << " " << a[start] << " " << a[end];
-	} else {
-		cout << 0 << " " << a[0] << " " << a[n-1];
-	}
-	
-}
+    int n;
+    scanf("%d", &n);
+    vector<int> v(n);
+    int leftindex = 0, rightindex = n - 1, sum = -1, temp = 0, tempindex = 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+        temp = temp + v[i];
+        if (temp < 0) {
+            temp = 0;
+            tempindex = i + 1;
+        } else if (temp > sum) {
+            sum = temp;
+            leftindex = tempindex;
+            rightindex = i;
+        }
+    }
+    if (sum < 0) sum = 0;
+    printf("%d %d %d", sum, v[leftindex], v[rightindex]);
+    return 0;
+} 
