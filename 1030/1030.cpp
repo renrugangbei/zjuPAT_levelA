84df0eb1668dcbc17593e9b1395cfe933211c5fd
renrugangbei <1183356044@qq.com>
2018-10-12 20:52:59 +0800

    10.12.18

--- 1030/1030.cpp
+++ 1030/1030.cpp
@@ -1,68 +1,75 @@
+#include<cstdio>
 #include<iostream>
 #include<vector>
-#include<cstdio>
-#define MAXV 501
-#define INF 0x3fffffff
+#define INF 0xfffffff
+#define MAXN 510
 using namespace std;
-int g[MAXV][MAXV],dis[MAXV],cost[MAXV][MAXV],costsum[MAXV]={INF},pre[MAXV];
-bool visit[MAXV]={false};
-void dijstra(int st){
-	fill(dis,dis+MAXV,INF);
-	fill(costsum,costsum+MAXV,INF);
-	dis[st]=0;
-	costsum[st]=0;
-	for(int i=0;i<MAXV;i++){
-		int u=-1,min=INF;
-		for(int j=0;j<MAXV;j++){
-			if(visit[j]==false&&dis[j]<min){
-				u=j;
-				min=dis[j]; 	
+int e[MAXN][MAXN],cost[MAXN][MAXN],dis[MAXN],minDis = INF, minCost = INF, start, ed;
+vector<int> pre[MAXN], temppath,path;
+bool visit[MAXN];
+void dij(int st){
+	dis[st] = 0;
+	for(int i = 0; i < MAXN; i++){
+		int u = -1,min = INF;
+		for(int j = 0; j < MAXN; j++){
+			if(dis[j] < min && visit [j] == false){
+				u = j;
+				min = dis[j];
 			}
 		}
-		if(u==-1) return;
-		visit[u]=true;
-		for(int k=0;k<MAXV;k++){
-			if(visit[k]==false&&g[u][k]!=INF){
-				if(dis[k]>dis[u]+g[u][k]){
-					pre[k]=u;
-					dis[k]=dis[u]+g[u][k];
-					costsum[k]=costsum[u]+cost[u][k];
-				}else if(dis[k]==dis[u]+g[u][k]){
-					if(costsum[k]>costsum[u]+cost[u][k]){
-						pre[k]=u;
-						costsum[k]=costsum[u]+cost[u][k];
-					}
+		if(u == -1)return;
+		visit[u] = true;
+		for(int v = 0; v < MAXN; v++){
+			if(e[u][v] != INF && visit[v] == false){
+				if(dis[v] > e[u][v] + dis[u]){
+					pre[v].clear();
+					pre[v].push_back(u);
+					dis[v] = e[u][v] + dis[u];
+				}else if(dis[v] == e[u][v] + dis[u]){
+					pre[v].push_back(u);
 				}
 			}
 		}
 	}
 }
-void DFS(int v,int st){
-	if(v==st){
-		printf("%d ",v);
+void dfs(int index){
+	temppath.push_back(index);
+	if(index == start){
+		int len = temppath.size();
+		int dis = 0,costt = 0;
+		for(int i = 0; i < len - 1; i++){
+			costt += cost[temppath[i]][temppath[i + 1]];
+		}
+		if(costt < minCost){
+			minCost = costt;
+			path = temppath;
+		}
+		temppath.pop_back();
 		return;
 	}
-	DFS(pre[v],st);
-	printf("%d ",v);
-} 
-int main(){
-	fill(g[0],g[0]+MAXV*MAXV,INF);
-	fill(cost[0],cost[0]+MAXV*MAXV,INF);
-	int citynum,roadnum,endcity,startcity;
-	scanf("%d %d %d %d",&citynum,&roadnum,&startcity,&endcity);
-	for(int i=0;i<roadnum;i++){
-		int city1,city2,tempdis,tempcost;
-		scanf("%d %d %d %d",&city1,&city2,&tempdis,&tempcost);
-		g[city1][city2]=tempdis;
-		g[city2][city1]=tempdis;
-		cost[city1][city2]=tempcost;
-		cost[city1][city2]=tempcost;
+	int len = pre[index].size();
+	for(int i = 0; i < len; i++){
+		dfs(pre[index][i]);
 	}
-	for(int i=0;i<MAXV;i++){
-			pre[i]=i;
-	}
-	dijstra(startcity);
-	DFS(endcity,startcity);
-	printf("%d %d",dis[endcity],costsum[endcity]);
+	temppath.pop_back();
+	return;
 }
-
+int main(){
+	fill(dis,dis+MAXN,INF);
+	fill(e[0],e[0]+MAXN*MAXN,INF);
+	fill(visit,visit+MAXN,false);
+	int n,m;
+	scanf("%d %d %d %d",&n,&m,&start,&ed);
+	for(int i = 0; i < m; i++){
+		int c1,c2, td, tc;
+		scanf("%d %d %d %d",&c1,&c2,&td,&tc);
+		e[c1][c2] = e[c2][c1] = td;
+		cost[c1][c2] = cost[c2][c1] = tc;  
+	}
+	dij(start);
+	dfs(ed);
+	for(int i = path.size() - 1; i >= 0; i--){
+		printf("%d ", path[i]);
+	}
+	printf("%d %d",dis[ed],minCost);
+} 
